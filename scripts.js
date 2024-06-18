ca7bf31f24b5a1a8fe056abb986db1968e0b6f44
Freehandtree <145225527+Freehandtree@users.noreply.github.com>
2024-06-18 12:06:46 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,6 +15,7 @@ const db = firebase.firestore();
 let username = "";
 
 function login() {
+    console.log("Login")
     username = document.getElementById('username').value;
     if (username) {
         document.getElementById('loginPage').style.display = 'none';
